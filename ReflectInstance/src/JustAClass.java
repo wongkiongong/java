f5b598a26c374a915e33a878682a65dbbbe7a4f7
root <root@huangqg-HP.(none)>
2016-06-05 20:51:30 +0800

    add project ReflectInstance

---
+++ ReflectInstance/src/JustAClass.java
@@ -0,0 +1,19 @@
+
+public class JustAClass {
+	
+	private String name = null;
+	
+	JustAClass(){}
+	
+	JustAClass(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+}
